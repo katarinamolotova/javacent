e73822b7c935e24f77389bef1463f0c14860606c
Mikhail Kukhtachev <101043013+Kuhtaaa@users.noreply.github.com>
2024-08-07 10:03:07 +0300

    Update PropertiesValidator.java

--- src/main/java/org/centrifugo/configarations/PropertiesValidator.java
+++ src/main/java/org/centrifugo/configarations/PropertiesValidator.java
@@ -5,7 +5,7 @@ public class PropertiesValidator {
     private final static int MIN_PORT = 1024;
     private final static int MAX_PORT = 49151;
 
-    private final static String LOCAL_REGEX = "^(https?://)?localhost$";
+    private final static String LOCALHOST_REGEX = "^(https?://)?localhost$";
     private final static String URL_REGEX = "^(https?://)?" +
                                             "((([a-z\\d]([a-z\\d-]*" +
                                             "[a-z\\d])*)\\.)+[a-z]{2,}" +
@@ -27,11 +27,11 @@ public class PropertiesValidator {
     }
 
     public static void urlValidate(final String url) {
-        if (!url.matches(LOCAL_REGEX) && !url.matches(URL_REGEX)) {
+        if (!url.matches(LOCALHOST_REGEX) && !url.matches(URL_REGEX)) {
             throw new IllegalStateException(
                     String.format(
                             "Illegal url value, value must match \n%s or \n%s",
-                            LOCAL_REGEX,
+                            LOCALHOST_REGEX,
                             URL_REGEX
                     ));
         }
